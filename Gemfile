cf0b7afe5b97d51f1b247b3958b15b2d7c7685f9
Karl Varga <kjvarga@gmail.com>
2010-10-21 12:30:04 -0700

    Get the specs running with a mock Rails 2.3.8 app

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 # A sample Gemfile
 source "http://rubygems.org"
 
-gem 'activerecord', '=2.3.8'
-gem 'activesupport', '=2.3.8'
+gem 'bundler', '>=1.0.0'
+gem 'rails', '2.3.8'
 gem 'sqlite3-ruby', '=1.3.1'
 
 gem 'will_paginate', '=2.3.15'
@@ -13,6 +13,7 @@ gem 'ruby-debug-base', '=0.10.3'
 gem 'machinist', '=1.0.6'
 gem 'faker', '=0.3.1'
 gem 'rspec', '=1.3.0', :require => 'spec'
+gem 'rspec-rails', '=1.3.2'
 gem 'test-unit', '=1.2.3'
 gem 'mocha', '=0.9.8'
 gem 'arid_cache', :path => "./"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,83 @@
+ENV["RAILS_ENV"] ||= 'test'
 root_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 $LOAD_PATH.unshift(File.join(root_path, '/test/lib')) # make requiring from test/lib easy
+
 
 require 'bundler/setup'
 Bundler.require
 
-require 'spec/autorun'
-require 'mock_rails'
+RAILS_DEFAULT_LOGGER = ENV["STDOUT"] ? Logger.new(STDOUT) : Logger.new(File.join(root_path, '/test/log/test.log'))
+RAILS_CACHE = ActiveSupport::Cache.lookup_store(:file_store, File.join(root_path, '/tmp/cache'))
+
+#load(File.join(root_path, sitemap_rails, 'Rakefile'))
+require File.join(root_path, 'spec', 'rails-2.3.8', 'config', 'boot')
+Rails.boot!
+
+require 'fix_active_support_file_store_expires_in'
+
+# Set loggers for all frameworks
+require 'active_record'
+require 'action_controller'
+for framework in ([ :active_record, :action_controller, :action_mailer ])
+  if Object.const_defined?(framework.to_s.camelize)
+    framework.to_s.camelize.constantize.const_get("Base").logger = Rails.logger
+  end
+end
+ActiveSupport::Dependencies.logger = Rails.logger
+Rails.cache.logger = Rails.logger
+debugger
+require 'db_prepare' #File.join(root_path, 'spec', 'rails-config', 'schema')
+
+#require 'spec/autorun'
+
+#require 'action_controller'
+#require 'active_support/test_case'
+#require 'active_record/fixtures' if defined?(ActiveRecord::Base)
+#require 'spec/test/unit'
+# require 'spec/rails/extensions/spec/runner/configuration'
+# require 'spec/rails/extensions/active_support/test_case'
+# require 'spec/rails/extensions/active_record/base'
+#ActiveSupport::TestCase.use_transactional_fixtures = true
+#Spec::Example::ExampleGroupFactory.default(ActiveSupport::TestCase)
+
+#require 'spec/rails'
+
+# require 'rack/utils'
+#
+# require 'action_controller/test_process'
+# require 'action_controller/integration'
+# require 'active_support/test_case'
+# require 'active_record/fixtures' if defined?(ActiveRecord::Base)
+# require 'spec/test/unit'
+# require 'spec/rails/matchers'
+# require 'spec/rails/mocks'
+# require 'spec/rails/example'
+# require 'spec/rails/extensions'
+# require 'spec/rails/interop/testcase'
+#
+# Spec::Example::ExampleGroupFactory.default(ActiveSupport::TestCase)
+#
+# if ActionView::Base.respond_to?(:cache_template_extensions)
+#   ActionView::Base.cache_template_extensions = false
+# end
+
+
+# require 'spec/autorun'
+#require 'mock_rails'
+require 'blueprint'
+AridCache.init_rails
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
-Spec::Runner.configure do |config|  
-  #include Blueprint::Helpers
+Spec::Runner.configure do |config|
   include ActiveRecordQueryMatchers
-  
   config.mock_with :mocha
-  
-  config.before(:all) do 
+  #config.use_transactional_fixtures = true
+  config.before(:all) do
     Sham.reset(:before_all)
   end
-  
-  config.before(:each) do 
+
+  config.before(:each) do
     Sham.reset(:before_each)
     full_example_description = "#{self.class.description} #{@method_name}"
     RAILS_DEFAULT_LOGGER.info("\n\n#{full_example_description}\n#{'-' * (full_example_description.length)}")
