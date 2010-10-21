30ea98a578ec9afc7eb97086f4e54cb4984c1ed3
Karl Varga <kjvarga@gmail.com>
2010-10-21 12:30:21 -0700

    Initial order spec

--- spec/arid_cache_spec.rb
+++ spec/arid_cache_spec.rb
@@ -1,7 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "AridCache" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe 'order' do
+    before :each do
+      @company1 = Company.make(:name => 'a')
+      @company2 = Company.make(:name => 'b')
+      Company.class_caches do
+        ordered_by_name { Company.all(:order => 'name ASC') }
+      end
+      #Company.clear_caches
+    end
+
+    it "should keep the original order with no order option" do
+      results = Company.cached_ordered_by_name
+      results.size.should == 2
+      results[0].name.should == @company1.name
+      results[1].name.should == @company2.name
+    end
+
+    it "should match the order option" do
+      results = Company.cached_ordered_by_name(:order => 'name DESC')
+      results.size.should == 2
+      results[0].name.should == @company2.name
+      results[1].name.should == @company1.name
+    end
   end
 end
